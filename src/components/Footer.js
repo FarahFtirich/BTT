65ba20c14ada17f80f85f013d06db59f76629102
FarahFtirich <105128737+FarahFtirich@users.noreply.github.com>
2024-02-22 16:17:36 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css'; 
+import logo from "../icons/logo.png";
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <div>footer </div>
-  )
-}
+    <footer className="footer">
+      <div className='links'>
+      <div className="footer__logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className='links2'>
+      <div className="footer__links">
+        <ul>
+          <li><a href="#">Researchers & BTT community</a></li><br/>
+          <li><a href="#">About BTT</a></li><br/>
+          <li><a href="#">News</a></li>
+        </ul>
+      </div>
+      <div className="footer__links">
+        <ul>
+          <li><a href="#">Our Team </a></li><br/>
+          <li><a href="#">(216) 00-00000</a></li><br/>
+          <li><a href="#">Contact us </a></li>
+        </ul>
+      </div>
+      </div>
+      </div>
+
+      <hr className="footer__line" />
+      <div className="footer__copy">
+        &copy; {new Date().getFullYear()} BTT. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
