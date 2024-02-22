c44271c5ba188f7b3e37dccc6a74a7a2fe8bcfe7
FarahFtirich <105128737+FarahFtirich@users.noreply.github.com>
2024-02-22 13:43:22 +0100

    update1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import './Navbar.css';
 import linkedin from '../icons/linkedin.png';
-import logo from './logo.png';
+import user from '../icons/user.png';
+import logo from "./logo.png";
+import { useState, useEffect } from 'react';
 
 
-export default function CNavbar() {
+
+  const CNavbar = () => {
+    const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+  const handleResize = () => {
+  setIsMobile(window.innerWidth <= 850); 
+      };
+  window.addEventListener('resize', handleResize);
+  handleResize();
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []); 
+return (
+  <div className={isMobile ? 'navbar-mobile' : 'navbar-desktop'}>
+    {isMobile ? (
+      <MobileNavbar />
+    ) : (
+      <DesktopNavbar />
+    )}
+  </div>
+);
+};
+
+const DesktopNavbar = () => {
   return (
   <div>
     <div className="navbar1">
@@ -14,16 +40,15 @@ export default function CNavbar() {
         <li><a href="#">EVENTS</a></li>
         <li><a href="#">CONTACT US </a></li>
         <li><a href="#">OUR TEAM</a></li>
-        <li><img href="#" src={linkedin.png}></img></li>
+        <li><img href="#" className='linkedin' src={linkedin}></img></li>
       </ul>
     </div>
     </div>
-    
-
+  
     <div className="navbar">
     <div className="left-section">
       <img
-        src={logo.png}
+        src={logo}
         alt="Logo"
         height="30"
         className="logo"
@@ -33,11 +58,22 @@ export default function CNavbar() {
       <ul className="nav-links">
         <li><a href="#">Researchers & BTT Community +</a></li>
         <li><a href="#">Industry & Entrepreneurs +</a></li>
-        <li><a href="#">About +</a></li>
+        <li><a href="#">About BTT +</a></li>
       </ul>
       <button className="btn">Submit Disclosure</button>
+      <img href="#" className='user' src={user}></img>
     </div>
     </div>
     </div> 
-  )
-}
+  );
+};
+const MobileNavbar = () => {
+  return (
+    <div>
+      <h1>hiii</h1>
+    </div>
+  );
+};
+
+
+export default CNavbar;
